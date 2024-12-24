f5f057233faf6cab5190c0efeff4dfc7b1c1c350
Emilse Arias <emilsearias@gmail.com>
2024-12-24 10:02:51 -0300

    agregamos los link funcional en el menu y en la pagina de error404

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,24 +1,25 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../../assets/Coffee_Logo.png";
+import { NavLink , Link} from "react-router";
 
 const Menu = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand>
+        <Link to={'/'} className="navbar-brand">
           <img
             src={logo}
             alt="logo Rolling Coffee"
             className="img-fluid"
             width={150}
           />
-        </Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end className='nav-link' to={'/'}>Inicio</NavLink>
+            <NavLink end className='nav-link'to={'/administrador'}>Administrador</NavLink>
+            <NavLink end className='nav-link' to={'/login'}>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,12 +1,13 @@
 import { Button } from "react-bootstrap";
 import error from "../../assets/error404.png";
+import { Link } from "react-router";
 
 const Error404 = () => {
   return (
     <section className="mainSection text-center">
       <img src={error} alt="error 404" />
       <div>
-        <Button className="btn btn-success" >Volver al inicio</Button>
+        <Link className="btn btn-success" to={'/'} >Volver al inicio</Link>
       </div>
     </section>
   );
