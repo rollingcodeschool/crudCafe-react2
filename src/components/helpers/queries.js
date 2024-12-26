e5975306b299d41a3fdb253066f688649f031808
Emilse Arias <emilsearias@gmail.com>
2024-12-26 09:46:58 -0300

    agregamos al archivo de queries la funcion para borrar un producto de la api

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,5 +1,7 @@
 // POST - crear un producto - 201
 // GET - obtener 1 o mas productos - 200
+// DELETE - borrar 1 producto
+// PUT - Editar 1 producto
 
 export const crearProductoAPI = async(productoNuevo)=>{
     try {
@@ -21,6 +23,19 @@ export const crearProductoAPI = async(productoNuevo)=>{
 export const listarProductosAPI = async()=>{
     try {
         const respuesta = await fetch('http://localhost:3000/productos')
+        console.log(respuesta)
+        return respuesta;
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+
+export const borrarProductoAPI = async(id)=>{
+    try {
+        const respuesta = await fetch('http://localhost:3000/productos/'+id,{
+            method:"DELETE"
+        })
         console.log(respuesta)
         return respuesta;
     } catch (error) {
