15a768e119c8bea6606a165b7977bfacb275f784
Emilse Arias <emilsearias@gmail.com>
2024-12-31 10:56:54 -0300

    agregamos la informacion del producto correspondiente en detalle de producto

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router";
+import { obtenerProductoAPI } from "../helpers/queries";
 
 const DetalleProducto = () => {
+  const {id} = useParams();
+  const [producto, setProducto] = useState({});
+
+  useEffect(()=>{
+    obtenerProducto();
+  },[])
+
+  const obtenerProducto = async()=>{
+    const respuesta = await obtenerProductoAPI(id)
+    if(respuesta.status === 200){
+      const datos = await respuesta.json()
+      setProducto(datos)
+    }else{
+      alert('Ocurrio un error intente este paso en unos minutos')
+    }
+  }
+
+
   return (
     <Container className="my-3 mainSection">
       <Card>
@@ -8,20 +29,20 @@ const DetalleProducto = () => {
           <Col md={6}>
             <Card.Img
               variant="top"
-              src="https://images.pexels.com/photos/414555/pexels-photo-414555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={producto.imagen}
+              alt={producto.nombreProducto}
             />
           </Col>
           <Col md={6}>
             <Card.Body>
-              <Card.Title className="primary-font">Capuchino</Card.Title>
+              <Card.Title className="primary-font">{producto.nombreProducto}</Card.Title>
               <hr />
               <Card.Text>
-              El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día.
+              {producto.descripcion_amplia}
               <br/>
-              <br/>
-              <span className="primary-font fw-semibold ">Categoria:</span> Infusiones
+              <span className="primary-font fw-semibold ">Categoria:</span> {producto.categoria}
               <br className='mb-3'/>
-              <span className="primary-font fw-semibold ">Precio: $1000</span></Card.Text>
+              <span className="primary-font fw-semibold ">Precio: ${producto.precio}</span></Card.Text>
             </Card.Body>
           </Col>
         </Row>
